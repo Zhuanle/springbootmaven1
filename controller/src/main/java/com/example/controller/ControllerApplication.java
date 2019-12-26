76639901b29759ab94ca1489c348ba5365694f96
Zhuanle <45566523+Zhuanle@users.noreply.github.com>
2019-12-26 16:15:10 +0800

    Update ControllerApplication.java

--- controller/src/main/java/com/example/controller/ControllerApplication.java
+++ controller/src/main/java/com/example/controller/ControllerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@MapperScan("com.example.mapper")
+//@MapperScan("com.example.mapper")
 //@ComponentScan("com.example")
 @SpringBootApplication public class ControllerApplication {
 
